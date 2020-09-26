7758cfe9df412dca0b0d652c2d1d1344460075bb
me <abdouder97@gmail.com>
2020-09-26 13:34:05 +0100

    add help page

---
+++ lib/pages/helpscreen.dart
@@ -0,0 +1,148 @@
+import 'dart:io';
+
+import 'package:dcsmobile/commons/FECommons.dart';
+import 'package:dcsmobile/commons/FEDrawer.dart';
+import 'package:dcsmobile/commons/fancyappbar.dart';
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class HelpScreen extends StatefulWidget {
+
+  final _title;
+
+  HelpScreen(this._title) : super();
+
+  @override
+  _HelpScreenState createState() => _HelpScreenState(this._title);
+}
+
+class _HelpScreenState extends State<HelpScreen> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+  final _title;
+
+  _HelpScreenState(this._title);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: _scaffoldKey,
+      appBar: AppBar(
+        backgroundColor: Colors.deepOrange,
+        title: Text('$_title'),
+      ),
+      backgroundColor: Colors.white,
+      drawer: FEDrawer(),
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 8.0),
+                child: Container(
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.height / 2,
+                    color: Colors.white,
+                    child: Image.asset("assets/images/help.png")),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width / 2,
+                child: RaisedButton(
+                  color: Colors.red,
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Icon(
+                        Icons.message,
+                        color: Colors.white,
+                        size: MediaQuery.of(context).size.shortestSide < 600
+                            ? 24
+                            : 30,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 8.0),
+                        child: Text(
+                          'envoyer un email',
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  onPressed: () => setState(() {
+                    _launchInBrowser("mailto://test@gmail.com");
+                  }),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width / 2,
+                child: RaisedButton(
+                  color: Colors.green,
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Icon(
+                        Icons.call,
+                        color: Colors.white,
+                        size: MediaQuery.of(context).size.shortestSide < 600
+                            ? 24
+                            : 30,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 8.0),
+                        child: Text(
+                          'Whatssap',
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  onPressed: () => setState(() {
+                    _launchWhatsApp(phone: "+21260022449922");
+                  }),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Future<void> _launchInBrowser(String url) async {
+    if (await canLaunch(url)) {
+      await launch(
+        url,
+        forceSafariVC: false,
+        forceWebView: false,
+        headers: <String, String>{'my_header_key': 'my_header_value'},
+      );
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
+  Future<void> _launchWhatsApp({@required String phone}) async {
+    String url() {
+      if (Platform.isIOS) {
+        return "whatsapp://wa.me/$phone";
+      } else {
+        return "whatsapp://send?phone=$phone";
+      }
+    }
+
+    if (await canLaunch(url())) {
+      await launch(url());
+    } else {
+      throw 'Could not launch ${url()}';
+    }
+  }
+}
